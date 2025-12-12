217847ac7c7a898ba546f619fcc9bcb45f7112d4
adelsonsljunior <junioradelsonst@gmail.com>
2025-12-12 00:57:52 -0300

    enable http and https ports and disable servicelb

--- scripts/master.sh
+++ scripts/master.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-sudo apt-get update -y
-sudo apt-get install ufw curl -y
+apt-get update -y
+apt-get install ufw curl -y
 
 ufw allow 6443/tcp #apiserver
 ufw allow from 10.42.0.0/16 to any #pods
 ufw allow from 10.43.0.0/16 to any #services
+ufw allow 80/tcp
+ufw allow 443/tcp
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip 192.168.56.10 --tls-san 192.168.56.10" sh -
+curl -sfL https://get.k3s.io | \
+    INSTALL_K3S_EXEC="--disable servicelb --node-ip 192.168.56.10 --tls-san 192.168.56.10" \
+    sh -
 
 cat /var/lib/rancher/k3s/server/node-token > /vagrant/token
 
--- scripts/worker.sh
+++ scripts/worker.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-sudo apt-get update -y
-sudo apt-get install ufw curl -y
+echo "IP: ${NODE_IP}"
+
+apt-get update -y
+apt-get install ufw curl -y
 
 ufw allow 6443/tcp #apiserver
 ufw allow from 10.42.0.0/16 to any #pods
 ufw allow from 10.43.0.0/16 to any #services
+ufw allow 80/tcp
+ufw allow 443/tcp
 
 TOKEN=$(cat /vagrant/token)
 
 curl -sfL https://get.k3s.io | \
     K3S_URL=https://192.168.56.10:6443 \
     K3S_TOKEN="$TOKEN" \
-    INSTALL_K3S_EXEC="--node-ip ${NODE_IP}" \
-    sh -
+    INSTALL_K3S_EXEC="--node-ip ${NODE_IP}" sh -
