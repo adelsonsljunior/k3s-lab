308e706c4e72b8d47e2f35c7e0cbddb89d12b618
adelsonsljunior <junioradelsonst@gmail.com>
2026-01-04 20:33:29 -0300

    fix: force k3s to use eth1 interface for multi-node networking

--- scripts/master.sh
+++ scripts/master.sh
@@ -10,12 +10,16 @@ ufw allow 80/tcp
 ufw allow 443/tcp
 
 curl -sfL https://get.k3s.io | \
-    INSTALL_K3S_EXEC="--disable servicelb --disable traefik --node-ip 192.168.56.10 --tls-san 192.168.56.10" \
+    INSTALL_K3S_EXEC="--disable servicelb \
+    --disable traefik \
+    --node-ip 192.168.56.10 \
+    --advertise-address 192.168.56.10 \
+    --flannel-iface=eth1 \
+    --tls-san 192.168.56.10" \
     sh -
 
 kubectl apply --server-side -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.1/standard-install.yaml
 
 cat /var/lib/rancher/k3s/server/node-token > /vagrant/token
-
 cat /etc/rancher/k3s/k3s.yaml > /vagrant/kubeconfig.lab
 sed -i 's/127\.0\.0\.1/192.168.56.10/g' /vagrant/kubeconfig.lab
--- scripts/worker.sh
+++ scripts/worker.sh
@@ -16,4 +16,4 @@ TOKEN=$(cat /vagrant/token)
 curl -sfL https://get.k3s.io | \
     K3S_URL=https://192.168.56.10:6443 \
     K3S_TOKEN="$TOKEN" \
-    INSTALL_K3S_EXEC="--node-ip ${NODE_IP}" sh -
+    INSTALL_K3S_EXEC="--node-ip ${NODE_IP} --flannel-iface=eth1" sh -
